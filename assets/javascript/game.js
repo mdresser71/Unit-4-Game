f98677fce8aecc1710b659b98d6eb4418a22f13b
MikeDr <mdresser71@gmail.com>
2018-09-03 21:05:50 -0500

    updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -27,9 +27,8 @@ var characters = {
       counterAttackPower: 25,
     },
   
-    
-  snake:{
-  name: "Snake Plissken",
+    snake:{
+      name: "Snake Plissken",
       health: 185,
       attackPower: 1,
       counterAttackPower: 25,
@@ -118,7 +117,456 @@ var characters = {
     $("#restart").hide();
     $("#game-message").empty();
   
-    // Determine which character the user has clicked
-    
-    
-    //$("#wyatt").<span cl
+    //Character Selection
+    $("#wyatt").on("click", function () {
+
+
+        console.log("Wyatt Earp is selected");
+        
+        
+        
+        // User is choosing the character
+        
+        if(characterSelected === false) {
+        
+        $("#game-message").empty();
+        
+        
+        
+        // Set the user's character
+        
+        initializeCharacter(characters.wyatt);
+        
+        characterSelected = true;
+        
+        
+        
+        // Display the chosen character
+        
+        $("#wyatt").removeClass("available-character").addClass("chosen-character");
+        
+        $("#chosen-character").append(this);
+        
+        
+        
+        // Move the remaining characters to the enemies section
+        
+        moveToEnemies();
+        
+        
+        
+        } else if ((characterSelected === true) && (defenderSelected === false)) {
+        
+        // User is choosing the defender
+        
+        if($("#wyatt").hasClass("enemy-character")) {
+        
+        $("#game-message").empty();
+        
+        
+        
+        // Set the user's enemy
+        
+        initializeDefender(characters.wyatt);
+        
+        defenderSelected = true;
+        
+        
+        
+        // Add the character to the defender section
+        
+        $("#wyatt").removeClass("enemy-character").addClass("defender-character");
+        
+        $("#defender-div").append(this);
+        
+        }
+        
+        }
+        
+        });
+        
+        
+        
+        $("#jack").on("click", function () {
+        
+        console.log("Jack Burton is selected");
+        
+        
+        
+        // User is choosing the character
+        
+        if(characterSelected === false) {
+        
+        $("#game-message").empty();
+        
+        
+        
+        // Set the user's character
+        
+        initializeCharacter(characters.jack);
+        
+        characterSelected = true;
+        
+        
+        
+        // Display the chosen character
+        
+        $("#jack").removeClass("available-character").addClass("chosen-character");
+        
+        $("#chosen-character").append(this);
+        
+        
+        
+        // Move the remaining characters to the enemies section
+        
+        moveToEnemies();
+        
+        
+        
+        } else if ((characterSelected === true) && (defenderSelected === false)) {
+        
+        // User is choosing the defender
+        
+        if($("#jack").hasClass("enemy-character")) {
+        
+        $("#game-message").empty();
+        
+        
+        
+        // Set the user's enemy
+        
+        initializeDefender(characters.jack);
+        
+        defenderSelected = true;
+        
+        
+        
+        // Add the character to the defender section
+        
+        $("#jack").removeClass("enemy-character").addClass("defender-character");
+        
+        $("#defender-div").append(this);
+        
+        }
+        
+        }
+        
+        });
+        
+        
+        
+        $("#john").on("click", function () {
+        
+        console.log("John Ruth is selected");
+        
+        
+        
+        // User is choosing the character
+        
+        if(characterSelected === false) {
+        
+        $("#game-message").empty();
+        
+        
+        
+        // Set the user's character
+        
+        initializeCharacter(characters.john);
+        
+        characterSelected = true;
+        
+        
+        
+        // Display the chosen character
+        
+        $("#john").removeClass("available-character").addClass("chosen-character");
+        
+        $("#chosen-character").append(this);
+        
+        
+        
+        // Move the remaining characters to the enemies section
+        
+        moveToEnemies();
+        
+        
+        
+        } else if ((characterSelected === true) && (defenderSelected === false)) {
+        
+        // User is choosing the defender
+        
+        if($("#john").hasClass("enemy-character")) {
+        
+        $("#game-message").empty();
+        
+        
+        
+        // Set the user's enemy
+        
+        initializeDefender(characters.john);
+        
+        defenderSelected = true;
+        
+        
+        
+        // Add the character to the defender section
+        
+        $("#john").removeClass("enemy-character").addClass("defender-character");
+        
+        $("#defender-div").append(this);
+        
+        }
+        
+        }
+        
+        });
+        
+        
+        
+        $("#macready").on("click", function () {
+        
+        console.log("RJ MacReady is selected");
+        
+        
+        
+        // User is choosing the character
+        
+        if(characterSelected == false) {
+        
+        $("#game-message").empty();
+        
+        
+        
+        // Set the user's character
+        
+        initializeCharacter(characters.macready);
+        
+        characterSelected = true;
+        
+        
+        
+        // Display the chosen character
+        
+        $("#macready").removeClass("available-character").addClass("chosen-character");
+        
+        $("#chosen-character").append(this);
+        
+        
+        
+        // Move the remaining characters to the enemies section
+        
+        moveToEnemies();
+        
+        
+        
+        } else if ((characterSelected === true) && (defenderSelected == false)) {
+        
+        // User is choosing the defender
+        
+        if($("#macready").hasClass("enemy-character")) {
+        
+        $("#game-message").empty();
+        
+        
+        
+        // Set the user's enemy
+        
+        initializeDefender(characters.macready);
+        
+        defenderSelected = true;
+        
+        
+        
+        // Add the character to the defender section
+        
+        $("#macready").removeClass("enemy-character").addClass("defender-character");
+        
+        $("#defender-div").append(this);
+        
+        }
+        
+        }
+        
+        });
+        
+        $("#snake").on("click", function () {
+        
+        
+        console.log("Snake Plissken is selected");
+        
+        
+        
+        // User is choosing the character
+        
+        if(characterSelected === false) {
+        
+        $("#game-message").empty();
+        
+        
+        
+        // Set the user's character
+        
+        initializeCharacter(characters.snake);
+        
+        characterSelected = true;
+        
+        
+        
+        // Display the chosen character
+        
+        $("#snake").removeClass("available-character").addClass("chosen-character");
+        
+        $("#chosen-character").append(this);
+        
+        
+        
+        // Move the remaining characters to the enemies section
+        
+        moveToEnemies();
+        
+        
+        
+        } else if ((characterSelected === true) && (defenderSelected === false)) {
+        
+        // User is choosing the defender
+        
+        if($("#snake").hasClass("enemy-character")) {
+        
+        $("#game-message").empty();
+        
+        
+        
+        // Set the user's enemy
+        
+        initializeDefender(characters.snake);
+        
+        defenderSelected = true;
+        
+        
+        
+        // Add the character to the defender section
+        
+        $("#snake").removeClass("enemy-character").addClass("defender-character");
+        
+        $("#defender-div").append(this);
+        
+        }
+        
+        }
+        
+        });
+        
+        
+        
+        //Attack button is clicked
+        
+        $("#attack-btn").on("click", function() {
+        
+        console.log("Attack selected");
+        
+        
+        
+        // User is ready to attack the defender
+        
+        if (characterSelected && defenderSelected && !gameOver) {
+        
+        // User attacks the defender and decreases the defender's health points
+        
+        defender.health = defender.health - character.attackPower;
+        
+        $(".defender-character").children(".health").html(defender.health);
+        
+        $("#game-message").html("<p>You attacked " + defender.name + " for " + character.attackPower + " damage.<p>");
+        
+        
+        
+        // User's attack power increases
+        
+        character.attackPower = character.attackPower + character.attackPower;
+        
+        
+        
+        // If defender is still alive, they counter attack the user
+        
+        if (defender.health > 0) {
+        
+        character.health = character.health - defender.counterAttackPower;
+        
+        $(".chosen-character").children(".health").html(character.health);
+        
+        
+        
+        // Check if the user survives the attack
+        
+        if (character.health > 0) {
+        
+        $("#game-message").append("<p>" + defender.name + " attacked you back for " + defender.counterAttackPower + " damage.</p>");
+        
+        } else {
+        
+        gameOver = true;
+        
+        $("#game-message").html("<p>You were defeated...</p><p>Play again?</p>");
+        
+        $("#restart").show();
+        
+        }
+        
+        } else {
+        
+        // Defender is defeated
+        
+        enemiesDefeated++;
+        
+        defenderSelected = false;
+        
+        $("#game-message").html("<p>You have defeated " + defender.name + ". Choose another enemy.</p>");
+        
+        $(".defender-character").hide();
+        
+        
+        
+        // Check if the user has won the game
+        
+        if (enemiesDefeated === 3) {
+        
+        gameOver = true;
+        
+        $("#game-message").html("<p>You have won the game!!!</p><p>Play again?</p>");
+        
+        $("#restart").show();
+        
+        }
+        
+        }
+        
+        } else if (!characterSelected && !gameOver) {
+        
+        $("#game-message").html("<p>You must first select your game character.</p>");
+        
+        } else if (!defenderSelected && !gameOver) {
+        
+        $("#game-message").html("<p>You must choose an enemy to fight.</p>");
+        
+        }
+        
+        
+        
+        });
+        
+        
+        
+        $("#restart").on("click", function() {
+        
+        console.log("Restart selected");
+        
+        
+        
+        resetGame();
+        
+        });
+        
+        
+        
+        });
+        
